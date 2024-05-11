177af1f2c98b3510c7fa7af3239da01251894233
Christianib003 <christianib003@gmail.com>
2024-05-11 11:52:40 +0200

    refactor do_quit to return True

--- console.py
+++ console.py
@@ -1,15 +1,15 @@
 #! /usr/bin/python3
-import sys
+
 from cmd import Cmd
 
 class Interpreter(Cmd):
     """This is a command line interpreter for interacting with the program"""
     
     def do_quit(self, input):
-        """handles the quiting of the application when user types <quit>"""
-        sys.exit()
+        """handle the quiting of the interpreter when user types 'quit' command"""
+        return True
 
-
+    
 if __name__ == "__main__":
     """Continuously prompt the user for input"""
     interpreter = Interpreter()
