e5fee70c3850617b18cc3519e6df9d35996bcec9
Christianib003 <christianib003@gmail.com>
2024-05-11 12:02:10 +0200

    refactor do_EOF to print new line before quiting

--- console.py
+++ console.py
@@ -2,7 +2,7 @@
 
 from cmd import Cmd
 
-
+prompt = 'hbnb'
 class Interpreter(Cmd):
     """This is a command line interpreter for interacting with the program"""
     
@@ -12,6 +12,7 @@ class Interpreter(Cmd):
     
     def do_EOF(self, input):
         """Handle end-of-file condition. eg: user pressing 'ctrl + D'."""
+        print("\n")
         return True
     
 if __name__ == "__main__":
