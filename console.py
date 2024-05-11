e444d920ed803c0cc1644c5b32dcec500433813f
Christianib003 <christianib003@gmail.com>
2024-05-11 11:47:24 +0200

    handle quiting the interpreter with 'quit' command

--- console.py
+++ console.py
@@ -1,12 +1,16 @@
 #! /usr/bin/python3
-
+import sys
 from cmd import Cmd
 
 class Interpreter(Cmd):
     """This is a command line interpreter for interacting with the program"""
-    pass
+    
+    def do_quit(self, input):
+        """handles the quiting of the application when user types <quit>"""
+        sys.exit()
 
 
 if __name__ == "__main__":
     """Continuously prompt the user for input"""
-    Interpreter.cmdloop()
+    interpreter = Interpreter()
+    interpreter.cmdloop()
