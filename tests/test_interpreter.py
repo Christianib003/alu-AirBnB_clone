f6c003912ee17a096d37e9fc886b25129552e013
Christianib003 <christianib003@gmail.com>
2024-05-11 14:01:19 +0200

    create class TestInterpreter

---
+++ tests/test_interpreter.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+import unittest
+from console import Interpreter
+
+class TestInterpreter(unittest.TestCase):
+    """Test the console interpreter"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
