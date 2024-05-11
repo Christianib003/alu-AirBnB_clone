fb8ffde62511ba1513d012032cb93e95355d88ec
Christianib003 <christianib003@gmail.com>
2024-05-11 14:13:55 +0200

    add test for emptyline method

--- tests/test_interpreter.py
+++ tests/test_interpreter.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 
 import unittest
+from unittest.mock import patch
+from io import StringIO
 from console import Interpreter
 
 class TestInterpreter(unittest.TestCase):
@@ -9,6 +11,12 @@ class TestInterpreter(unittest.TestCase):
     def setUp(self):
         """Create a new Interpreter instance before each test case"""
         self.interpreter = Interpreter()
+
+    def test_emptyline(self):
+        """Test that emptyline method shifts the cursor to new line"""
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            self.interpreter.emptyline()
+            self.assertEqual(fake_out.getvalue(), "")
 
 
 if __name__ == "__main__":
