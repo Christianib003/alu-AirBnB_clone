c25afc66cd8b4f4f31ff34a2fa0d9f8d7dc059aa
Christianib003 <christianib003@gmail.com>
2024-05-13 18:55:04 +0200

    create class TestBaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,3 +4,11 @@ import unittest
 from datetime import datetime
 
 from models.base_model import BaseModel
+
+
+class TestBaseModel(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
