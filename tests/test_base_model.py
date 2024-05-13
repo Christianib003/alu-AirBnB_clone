bda4d4ad484b801ba28f1acd49177d631b861d19
Christianib003 <christianib003@gmail.com>
2024-05-13 19:01:38 +0200

    add unit test  method __init__ of BaseModel

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -17,6 +17,20 @@ class TestBaseModel(unittest.TestCase):
         """Clean up BaseModel instance after testing"""
         del self.base_model_instance
     
+    def test_init(self):
+        """Test BaseModel instance initialization"""
+        # Test if the attributes are set after initialization
+        self.assertIsNotNone(self.base_model_instance.id)
+        self.assertIsNotNone(self.base_model_instance.created_at)
+        self.assertIsNotNone(self.base_model_instance.updated_at)
+
+        # Test if the id is a string
+        self.assertIsInstance(self.model.id, str)
+
+        # Test if created_at and updated_at are datetime objects
+        self.assertIsInstance(self.base_model_instance.created_at, datetime)
+        self.assertIsInstance(self.base_model_instance.updated_at, datetime)
+    
 
 if __name__ == "__main__":
     unittest.main()
