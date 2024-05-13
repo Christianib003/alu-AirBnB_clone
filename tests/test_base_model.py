d31080448bf9d505384e3ebb23246e867bb8ce5c
Christianib003 <christianib003@gmail.com>
2024-05-13 18:59:28 +0200

    create method tearDown() to delete the BaseModel instance after each test case

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -13,6 +13,10 @@ class TestBaseModel(unittest.TestCase):
         """Set up BaseModel instance for testing"""
         self.base_model_instance = BaseModel()
     
+    def tearDown(self):
+        """Clean up BaseModel instance after testing"""
+        del self.base_model_instance
+    
 
 if __name__ == "__main__":
     unittest.main()
