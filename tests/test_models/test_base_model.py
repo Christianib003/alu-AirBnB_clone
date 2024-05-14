2df5dc848bac75e6d27205e90f2f85d885d59738
Christianib003 <christianib003@gmail.com>
2024-05-14 08:54:43 +0200

    correct error in unit test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -40,8 +40,8 @@ class TestBaseModel(unittest.TestCase):
         }
         model_with_kwargs = BaseModel(**kwargs)
         self.assertEqual(model_with_kwargs.id, "123")
-        self.assertEqual(model_with_kwargs.created_at, datetime.strptime("2022-01-01T00:00:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
-        self.assertEqual(model_with_kwargs.updated_at, datetime.strptime("2022-01-01T00:00:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertEqual(model_with_kwargs.created_at, datetime.strptime("2024-05-14T08:25:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertEqual(model_with_kwargs.updated_at, datetime.strptime("2024-05-14T08:25:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
         self.assertFalse(hasattr(model_with_kwargs, "__class__"))
 
     
