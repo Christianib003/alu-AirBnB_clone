113f3a753ba228da68f3e5dd23b965de58ca5da3
Christianib003 <christianib003@gmail.com>
2024-05-14 16:43:16 +0200

    add method setUp to initialize testing dependencies

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -2,8 +2,16 @@
 
 import unittest
 
+from models.engine.file_storage import FileStorage
+
 class TestFileStorage(unittest.TestCase):
-    pass
+    """Test the FileStorage class."""
+    
+    def setUp(self):
+        """Setup test dependencies."""
+        self.storage = FileStorage()
+        self.test_file = "test_file.json"
+        self.storage._FileStorage__file_path = self.test_file
 
 if __name__ == '__main__':
     unittest.main()
