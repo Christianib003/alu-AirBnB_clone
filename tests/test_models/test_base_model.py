2a9d7525a220fa017e9e44b15d68cbb01fa1e6b1
Christianib003 <christianib003@gmail.com>
2024-05-14 08:37:14 +0200

    test if the __class__key of provided kwargs is not used when creating an instance

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -42,6 +42,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(model_with_kwargs.id, "123")
         self.assertEqual(model_with_kwargs.created_at, datetime.strptime("2022-01-01T00:00:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
         self.assertEqual(model_with_kwargs.updated_at, datetime.strptime("2022-01-01T00:00:00.000000", "%Y-%m-%dT%H:%M:%S.%f"))
+        self.assertFalse(hasattr(model_with_kwargs, "__class__")) # key __class__ should not be used when creating an instance
 
     
     def test_save(self):
