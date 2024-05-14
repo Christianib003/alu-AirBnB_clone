909ae6a3e1ad25f21c3e07d43bbe9f5398c9a0ca
Christianib003 <christianib003@gmail.com>
2024-05-14 16:45:24 +0200

    add method tearDown to cleanup all the files created by testing activities

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -1,17 +1,26 @@
 #! /usr/bin/python3
 
 import unittest
+import os
 
 from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     """Test the FileStorage class."""
-    
+
     def setUp(self):
         """Setup test dependencies."""
         self.storage = FileStorage()
         self.test_file = "test_file.json"
         self.storage._FileStorage__file_path = self.test_file
+
+    def tearDown(self):
+        """Clean up test effects."""
+        try:
+            os.remove(self.test_file)
+        except FileNotFoundError:
+            pass
+
 
 if __name__ == '__main__':
     unittest.main()
