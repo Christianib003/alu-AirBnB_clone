0b25bd6f490ad8ebf6b26c6a75bfe52daeb1fd90
Christianib003 <christianib003@gmail.com>
2024-05-15 09:32:05 +0200

    add setUp and tearDown method for testing FileStorage

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -5,7 +5,28 @@ from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
-    pass
+    """
+    This class contains the test cases for the FileStorage class.
+    """
+    def setUp(self):
+        """
+        This method Sets up the testing dependencies before each test case
+        """
+        self.storage = FileStorage()
+        self.obj = BaseModel()
+        self.storage.new(self.obj)
+
+    def tearDown(self):
+        """
+        This method cleans up the testing dependencies after each test case
+        """
+        del self.storage
+        del self.obj
+        try:
+            os.remove('file.json')
+        except FileNotFoundError:
+            pass
+
 
 if __name__ == '__main__':
     unittest.main()
