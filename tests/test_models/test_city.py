da82f319dd2619e57aa7d3ceadd933a786d1a530
Christianib003 <christianib003@gmail.com>
2024-05-16 18:44:52 +0200

    create test class for city class

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+
+import unittest
+from models.city import City
+from models.base_model import BaseModel
+
+
+class TestCity(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
