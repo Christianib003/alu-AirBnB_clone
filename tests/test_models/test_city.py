ffa4bea608af49b2ebe30a8f57a56677ffeb540f
Christianib003 <christianib003@gmail.com>
2024-05-16 18:50:36 +0200

    add test for verifying the conversion of City object to dictionary

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -55,5 +55,37 @@ class TestCity(unittest.TestCase):
         # Assert that City is a subclass of BaseModel
         self.assertTrue(issubclass(type(city), BaseModel))
 
+    def test_city_string_representation(self):
+        """
+        Tests the string representation of City.
+        """
+        # Create a new City instance
+        city = City()
+
+        # Assert that the string representation of City is correct
+        self.assertEqual(str(city), f"[City] ({city.id}) {city.__dict__}")
+
+    def test_city_to_dict(self):
+        """
+        Tests the to_dict method of City.
+        """
+        # Create a new City instance
+        city = City()
+
+        # Add values to City's class attributes
+        city.state_id = "123"
+        city.name = "Michigan"
+
+        # Get the dictionary representation of City
+        city_dict = city.to_dict()
+
+        # Assert that the dictionary representation has the correct keys
+        self.assertIn("id", city_dict)
+        self.assertIn("created_at", city_dict)
+        self.assertIn("updated_at", city_dict)
+        self.assertIn("__class__", city_dict)
+        self.assertIn("state_id", city_dict)
+        self.assertIn("name", city_dict)
+
 if __name__ == "__main__":
     unittest.main()
