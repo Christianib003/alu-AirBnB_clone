dac4bf36104e286f4cae2794814b62e25a97bfe0
Christianib003 <christianib003@gmail.com>
2024-05-16 19:20:46 +0200

    add setUp method for class TestPlace

--- tests/test_place.py
+++ tests/test_place.py
@@ -9,7 +9,20 @@ from models.base_model import BaseModel
 
 
 class TestPlace(unittest.TestCase):
-    pass
+    """
+    Unit test class for Place
+    """
+
+    def setUp(self):
+        """
+        Creates a temporary test file and saves it.
+        """
+        # Create a temporary test file
+        with open("test_file.json", "w") as f:
+            pass
+
+        # Save the file
+        storage.save()
 
 if __name__ == "__main__":
     unittest.main()
