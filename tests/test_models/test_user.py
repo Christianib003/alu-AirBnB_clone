4e227734c3e51c6ff890bb7f3523719e9e46e409
Christianib003 <christianib003@gmail.com>
2024-05-16 12:50:30 +0200

    add test for testing the conversion of  user object to dictionary

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -67,6 +67,35 @@ class TestUser(unittest.TestCase):
         # Assert that the string representation is formatted correctly
         self.assertEqual(user_str, f"[User] ({user.id}) {user.__dict__}")
 
+    def test_user_to_dict(self):
+        """
+        Tests the to_dict method of User.
+        """
+        # Create a new User instance
+        user = User()
+
+        # Set attributes for user
+        user.email = "example@testing.com"
+        user.first_name = "John"
+        user.last_name = "Doe"
+        user.password = "password"
+        user.save()
+
+        # Get the dictionary representation of User
+        user_dict = user.to_dict()
+
+        # Assert that the __class__ key value
+        self.assertEqual(user_dict["__class__"], "User")
+
+        # Assert that the dictionary contains all attributes of User
+        self.assertEqual(user_dict["id"], user.id)
+        self.assertEqual(user_dict["created_at"], user.created_at.isoformat())
+        self.assertEqual(user_dict["updated_at"], user.updated_at.isoformat())
+        self.assertEqual(user_dict["email"], user.email)
+        self.assertEqual(user_dict["password"], user.password)
+        self.assertEqual(user_dict["first_name"], user.first_name)
+        self.assertEqual(user_dict["last_name"], user.last_name)
+
 
 if __name__ == "__main__":
     unittest.main()
