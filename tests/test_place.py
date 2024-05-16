977e72d6fb2bd8dfa4f5205af55a648925030b7f
Christianib003 <christianib003@gmail.com>
2024-05-16 19:20:10 +0200

    create test class for class Place

---
+++ tests/test_place.py
@@ -0,0 +1,15 @@
+#! /usr/bin/python3
+
+import unittest
+import os
+
+from models import storage
+from models.place import Place
+from models.base_model import BaseModel
+
+
+class TestPlace(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
