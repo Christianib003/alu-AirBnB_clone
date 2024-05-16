60f4c8d5ec002f52bf922d53d8fc549dca24cf49
Christianib003 <christianib003@gmail.com>
2024-05-16 18:16:21 +0200

    create test class for State

---
+++ tests/test_models/test_state.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+import unittest
+import os
+
+from models import storage
+from models.state import State
+from models.base_model import BaseModel
+
+
+class TestState(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
