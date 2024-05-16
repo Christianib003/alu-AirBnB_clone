980ec1d9aab0a21f1ee25db15431eacac4e9ac68
Christianib003 <christianib003@gmail.com>
2024-05-16 19:06:11 +0200

    create test class for Amenity class

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python3
+
+import unittest
+import os
+
+from models import storage
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+class TestAmenity(unittest.TestCase):
+    """Unit test class for Amenity"""
+
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
