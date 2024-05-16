d7392f8c95f7ea9014b68d22664a34317b917ee1
Christianib003 <christianib003@gmail.com>
2024-05-16 19:04:36 +0200

    create Amenity class inheriting from BaseModel

---
+++ models/amenity.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+""" This module contains the Amenity class """
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """ This class defines Amenity class """
+
+    name = ""
