167e03c653adb13fd01a11fad0d7e8d0c7f9f65f
Christianib003 <christianib003@gmail.com>
2024-05-16 18:22:14 +0200

    add method setup for TestState

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -8,7 +8,21 @@ from models.base_model import BaseModel
 
 
 class TestState(unittest.TestCase):
-    pass
+    """
+    Unit test class for State model
+    """
+
+    def setUp(self):
+        """
+        Creates a temporary test file and saves it.
+        """
+        # Create a temporary test file
+        with open("test_file.json", "w") as f:
+            pass
+
+        # Save the file
+        storage.save()
+
 
 if __name__ == "__main__":
     unittest.main()
