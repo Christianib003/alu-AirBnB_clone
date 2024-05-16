c43c8087480b3b204918d78f3623f947eebbb9de
Christianib003 <christianib003@gmail.com>
2024-05-16 19:12:13 +0200

    import Amenity class into file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,6 +6,7 @@ from models.base_model import BaseModel
 from models.user import User
 from models.state import State
 from models.city import City
+from models.amenity import Amenity  
 
 class FileStorage:
     """
