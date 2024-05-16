eb45dfca8ead77fdf1b486274521fed0c9226224
Christianib003 <christianib003@gmail.com>
2024-05-16 19:06:35 +0200

    add setUp method for TestAmenity class

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -10,7 +10,16 @@ from models.base_model import BaseModel
 class TestAmenity(unittest.TestCase):
     """Unit test class for Amenity"""
 
-    pass
+    def setUp(self):
+        """
+        Creates a temporary test file and saves it.
+        """
+        # Create a temporary test file
+        with open("test_file.json", "w") as f:
+            pass
+
+        # Save the file
+        storage.save()
 
 if __name__ == "__main__":
     unittest.main()
