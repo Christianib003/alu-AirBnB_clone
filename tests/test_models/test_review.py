88e839d1edc352eb6ba150b935e42a332e2a4344
Christianib003 <christianib003@gmail.com>
2024-05-16 19:43:38 +0200

    create a test class for testing Review class

---
+++ tests/test_models/test_review.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+import unittest
+import os
+
+from models import storage
+from models.review import Review
+from models.base_model import BaseModel
+
+class TestReview(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
