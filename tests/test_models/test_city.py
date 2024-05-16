7539420bda19c408368f5d2dcab8e3bc45aa79ca
Christianib003 <christianib003@gmail.com>
2024-05-16 18:46:08 +0200

    create setUp method for TestCity

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -6,7 +6,20 @@ from models.base_model import BaseModel
 
 
 class TestCity(unittest.TestCase):
-    pass
+    """ 
+    Unit test class for City model
+    """
+
+    def setUp(self):
+        """
+        Creates a temporary test file and saves it.
+        """
+        # Create a temporary test file
+        with open("test_file.json", "w") as f:
+            pass
+
+        # Save the file
+        storage.save()
 
 if __name__ == "__main__":
     unittest.main()
