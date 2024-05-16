6677aec40b5d6c3f0a265354e8444207a9fcc335
Christianib003 <christianib003@gmail.com>
2024-05-16 19:38:12 +0200

    import Review class into file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,6 +8,7 @@ from models.state import State
 from models.city import City
 from models.amenity import Amenity 
 from models.place import Place
+from models.review import Review
 
 class FileStorage:
     """
