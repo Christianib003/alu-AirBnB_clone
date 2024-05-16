8ac84f6bec95e47eba076f5c365ee563baba0ac2
Christianib003 <christianib003@gmail.com>
2024-05-16 18:16:05 +0200

    create state class with name as class attribute

---
+++ models/state.py
@@ -0,0 +1,12 @@
+#! /usr/bin/python3
+""" 
+This modules contains the state class
+"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """ 
+    State class
+    """
+    name = ""
