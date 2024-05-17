5b862dbaefe517140b37db7eb6d7bab205123824
Christianib003 <christianib003@gmail.com>
2024-05-17 10:47:16 +0200

    remove linter errors in state.py

--- models/state.py
+++ models/state.py
@@ -1,12 +1,13 @@
 #! /usr/bin/python3
-""" 
+"""
 This modules contains the state class
 """
 
 from models.base_model import BaseModel
 
+
 class State(BaseModel):
-    """ 
+    """
     State class
     """
     name = ""
