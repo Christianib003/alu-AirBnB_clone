243a66d9fa7b00fa779ebb84fc079d850bbcfc83
Christianib003 <christianib003@gmail.com>
2024-05-17 10:51:52 +0200

    remove linter errors from review.py

--- models/review.py
+++ models/review.py
@@ -3,9 +3,10 @@
 
 from models.base_model import BaseModel
 
+
 class Review(BaseModel):
     """This class represents a review for a place"""
-    
+
     place_id = ""
     user_id = ""
     text = ""
