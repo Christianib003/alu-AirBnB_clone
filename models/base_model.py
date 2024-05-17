1dc7cf2fea0f25ca9b8d77b57d17a9bb25e8c204
Christianib003 <christianib003@gmail.com>
2024-05-17 10:28:56 +0200

    remove whitespaces in the empty lines in base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Initialize the Base model with three instance attributes"""
-        
+
         self.id = str(uuid.uuid4()) # It's a requirement to convert id to string
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -23,15 +23,15 @@ class BaseModel:
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
                 if key != "__class__":
                     setattr(self, key, value)
-            
+
         models.storage.new(self)
-    
+
     def save(self):
         """Updates the updated_at attribute to the current datetime when the object is updated"""
         self.updated_at = datetime.now()
         models.storage.save()
         return self.updated_at # return the updated "updated_at" attribute
-    
+
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance"""
         instance_dict = self.__dict__.copy() # Use copy() to avoid modifying the original dictionary
@@ -42,7 +42,7 @@ class BaseModel:
         instance_dict["created_at"] = self.created_at.isoformat()
         instance_dict["updated_at"] = self.updated_at.isoformat()
         return instance_dict
-    
+
     def __str__(self):
         """Returns a string representation of the instance"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
